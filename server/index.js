8584b36bf9f1f49e1c3a0def059c11b27c20d11e
Krishna-Aggarwal-11 <krishnaaggarwal11may@gmail.com>
2024-06-02 20:05:26 +0530

    connecting to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
